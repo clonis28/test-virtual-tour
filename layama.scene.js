2620b1488b66dfa82929ab8b3a634ac70cb9ff05
Claudio Lonis <c.lonis@lightmap.it>
2025-01-29 11:28:19 +0100

    update

---
+++ layama.scene.js
@@ -0,0 +1,82 @@
+// Created with Motiva Layama v2.2 2200 https://www.motivacg.com/layama
+// Rendered with Layama for 3dsmax - Corona:Corona version:11 (Hotfix 1)
+
+function getLayamaCameras()
+{
+   var layamaCameras = [];
+   layamaCameras.push({n: "VTN0000", a: "00_START", p: {x:-329.995, y:170, z:353.397}, l: {x:-328.995, y:170, z:353.404},f: [167.729,-0.0185188,0.999774,-0.0104329],c: [1,1,1,1,0,0,0,1,1,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]});
+   layamaCameras.push({n: "VTN0001", a: "CORRIDOIO_ENTRATA_001", p: {x:-189.041, y:170, z:353.397}, l: {x:-188.041, y:170, z:353.404}, f: [167.688,-0.0079062,0.999966,-0.00225049], c: [1,1,1,1,0,0,0,1,1,0,1,1,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0]});
+   layamaCameras.push({n: "VTN0002", a: "CORRIDOIO_ENTRATA_003", p: {x:-36.3488, y:170, z:353.397}, l: {x:-35.3488, y:170, z:353.404}, f: [167.789,0.00495313,0.999987,0.00144413], c: [1,1,1,1,1,1,1,1,1,0,1,1,0,1,0,1,0,1,1,1,0,0,1,1,0,0,0,0,0,0,0]});
+   layamaCameras.push({n: "VTN0003", a: "BAGNO_006", p: {x:-36.3488, y:170, z:417.633}, l: {x:-35.3488, y:170, z:417.641}, f: [168.144,-0.0525027,0.993136,0.104515], c: [1,1,1,1,1,1,1,1,0,0,1,0,0,1,1,1,0,1,0,1,0,0,1,0,0,0,0,0,0,0,0]});
+   layamaCameras.push({n: "VTN0004", a: "BAGNO_001", p: {x:-111.733, y:170, z:536.301}, l: {x:-110.733, y:170, z:536.308}, f: [168.394,-0.0137695,0.999817,0.0133113], c: [0,0,1,1,1,1,1,0,0,0,0,0,0,0,0,1,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0]});
+   layamaCameras.push({n: "VTN0005", a: "BAGNO_003", p: {x:2.91213, y:170, z:559.537}, l: {x:3.9121, y:170, z:559.544}, f: [168.493,-0.00482111,0.999923,0.0114435], c: [0,0,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]});
+   layamaCameras.push({n: "VTN0006", a: "BAGNO_005", p: {x:-33.246, y:170, z:483.318}, l: {x:-32.2461, y:170, z:483.325}, f: [168.251,0.107129,0.994056,0.0193726], c: [0,0,1,1,1,1,1,0,0,0,0,0,0,1,1,1,0,1,0,0,0,0,0,0,0,0,0,0,1,0,1]});
+   layamaCameras.push({n: "VTN0007", a: "CAMERA_000", p: {x:144.122, y:170, z:353.397}, l: {x:145.122, y:170, z:353.404}, f: [167.739,-0.00671945,0.999904,0.0121027], c: [1,1,1,1,0,0,0,1,1,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]});
+   layamaCameras.push({n: "VTN0008", a: "CAMERA_001", p: {x:208.73, y:170, z:398.147}, l: {x:209.73, y:170, z:398.155}, f: [167.853,0.00272025,0.999996,0.000123702], c: [1,1,1,0,0,0,0,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]});
+   layamaCameras.push({n: "VTN0009", a: "CAMERA_003", p: {x:208.73, y:170, z:535.266}, l: {x:209.73, y:170, z:535.273}, f: [167.871,0.00423899,0.999985,-0.00350605], c: [0,0,0,0,0,0,0,0,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]});
+   layamaCameras.push({n: "VTN0010", a: "CAMERA_005", p: {x:303.907, y:170, z:342.442}, l: {x:304.907, y:170, z:342.449}, f: [167.847,0.017617,0.9998,0.00941673], c: [1,1,1,1,0,0,0,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]});
+   layamaCameras.push({n: "VTN0011", a: "CAMERA_007", p: {x:424.511, y:170, z:393.807}, l: {x:425.511, y:170, z:393.815}, f: [168.716,0.0524531,0.996734,-0.061405], c: [1,1,1,0,0,0,0,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]});
+   layamaCameras.push({n: "VTN0012", a: "CAMERA_009", p: {x:441.544, y:170, z:496.831}, l: {x:442.544, y:170, z:496.838}, f: [167.911,-0.00123832,0.999999,0.000449351], c: [0,0,0,0,0,0,0,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]});
+   layamaCameras.push({n: "VTN0013", a: "CORRIDOIO_LIVING_000", p: {x:48.3819, y:170, z:134.981}, l: {x:49.3819, y:170, z:134.988}, f: [167.739,-0.00929406,0.999952,-0.0031996], c: [0,0,1,1,0,0,1,0,0,0,0,0,0,1,1,1,1,1,1,0,0,0,0,1,0,0,0,0,1,0,1]});
+   layamaCameras.push({n: "VTN0014", a: "CORRIDOIO_LIVING_002", p: {x:49.865, y:170, z:-20.0923}, l: {x:50.8649, y:170, z:-20.0848}, f: [167.829,0.000319425,0.99996,0.00892084], c: [0,0,0,1,0,0,1,0,0,0,0,0,0,1,1,0,0,1,0,0,0,0,0,0,0,0,0,1,1,0,1]});
+   layamaCameras.push({n: "VTN0015", a: "LIVING_GIAPPO_005", p: {x:198.176, y:170, z:88.7823}, l: {x:199.176, y:170, z:88.7897}, f: [167.76,0.0112514,0.999908,0.00759168], c: [0,0,1,1,1,0,1,0,0,0,0,0,0,1,0,1,1,1,1,0,0,0,0,1,0,0,0,0,0,0,0]});
+   layamaCameras.push({n: "VTN0016", a: "LIVING_GIAPPO_001", p: {x:415.373, y:170, z:151.174}, l: {x:416.373, y:170, z:151.181}, f: [167.965,0.00743472,0.999187,0.0396191], c: [0,1,0,0,0,0,0,0,0,0,0,0,0,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0]});
+   layamaCameras.push({n: "VTN0017", a: "CORRIDOIO_LIVING_003", p: {x:47.3188, y:170, z:281.496}, l: {x:48.3188, y:170, z:281.503}, f: [167.788,0.00794958,0.999942,-0.0072644], c: [0,1,1,1,1,0,1,0,0,0,0,0,0,1,1,1,1,1,1,1,0,0,1,1,0,0,0,0,1,0,1]});
+   layamaCameras.push({n: "VTN0018", a: "LIVING_GIAPPO_003", p: {x:351.054, y:170, z:82.5124}, l: {x:352.054, y:170, z:82.5199}, f: [167.817,-0.0110638,0.999929,-0.00446847], c: [0,0,1,0,0,0,0,0,0,0,0,0,0,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0]});
+   layamaCameras.push({n: "VTN0019", a: "CUCINA_005", p: {x:280.036, y:170, z:22.932}, l: {x:281.036, y:170, z:22.9394}, f: [167.941,0.0801133,0.92291,0.376589], c: [0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0]});
+   layamaCameras.push({n: "VTN0020", a: "CUCINA_000", p: {x:288.391, y:170, z:-86.7892}, l: {x:289.391, y:170, z:-86.7817}, f: [167.818,0.00904211,0.999959,0.000987229], c: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,1,1,1,1,1,1,0,1,1,0,0,0]});
+   layamaCameras.push({n: "VTN0021", a: "LIVING_TAVOLO_007", p: {x:282.253, y:170, z:-250.241}, l: {x:283.253, y:170, z:-250.234}, f: [167.759,-0.00235741,0.999996,0.00155469], c: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,1,1,1,1,1,0,1,1,1,1,1,1]});
+   layamaCameras.push({n: "VTN0022", a: "CUCINA_002", p: {x:464.702, y:170, z:-184.397}, l: {x:465.702, y:170, z:-184.389}, f: [167.593,-0.00295088,0.999952,-0.00936128], c: [0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0]});
+   layamaCameras.push({n: "VTN0023", a: "CUCINA_004", p: {x:467.325, y:170, z:-4.39336}, l: {x:468.325, y:170, z:-4.3859}, f: [167.833,0.00445584,0.999978,0.00485857], c: [0,0,1,0,0,0,0,0,0,0,0,0,0,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0]});
+   layamaCameras.push({n: "VTN0024", a: "LIVING_TAVOLO_002", p: {x:462.501, y:170, z:-356.073}, l: {x:463.501, y:170, z:-356.066}, f: [167.83,0.00665507,0.999924,-0.0104151], c: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,1,1,0,1,1,1,1,1,0,1,1,1]});
+   layamaCameras.push({n: "VTN0025", a: "LIVING_TAVOLO_003", p: {x:456.388, y:170, z:-469.629}, l: {x:457.388, y:170, z:-469.622}, f: [167.841,-0.0115128,0.999933,0.00150031], c: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,1,0,1,1,1,1,1,1,1,1,1,0]});
+   layamaCameras.push({n: "VTN0026", a: "LIVING_TAVOLO_005", p: {x:281.727, y:170, z:-424.635}, l: {x:282.727, y:170, z:-424.627}, f: [167.808,0.0258375,0.999661,-0.00316798], c: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1]});
+   layamaCameras.push({n: "VTN0027", a: "LIVING_DIVANO_005", p: {x:169.71, y:170, z:-399.767}, l: {x:170.71, y:170, z:-399.76}, f: [167.445,-0.0180101,0.999832,-0.00332749], c: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,1,0,1,1,1,1,0,1,1,1,1,1,1]});
+   layamaCameras.push({n: "VTN0028", a: "LIVING_DIVANO_001", p: {x:57.216, y:170, z:-323.555}, l: {x:58.216, y:170, z:-323.547}, f: [167.253,0.00382722,0.999657,0.0258897], c: [0,0,0,0,0,0,1,0,0,0,0,0,0,1,1,0,0,1,0,0,0,1,1,0,1,1,1,1,1,1,1]});
+   layamaCameras.push({n: "VTN0029", a: "LIVING_DIVANO_003", p: {x:153.821, y:170, z:-217.547}, l: {x:154.821, y:170, z:-217.54}, f: [167.883,0.00203893,0.999985,0.00503811], c: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,1,1,1,1,1,1,1]});
+   layamaCameras.push({n: "VTN0030", a: "LIVING_DIVANO_004", p: {x:50.0011, y:170, z:-191.448}, l: {x:51.0011, y:170, z:-191.44}, f: [167.877,-0.00429372,0.999991,-0.000243228], c: [0,0,0,0,0,0,1,0,0,0,0,0,0,1,1,0,0,1,0,0,0,1,0,0,1,0,1,1,1,1,1]});
+   return layamaCameras;
+}
+
+function getLayamaResolutions()
+{
+   var layamaResolutions = [];
+   layamaResolutions.push("2048");
+   layamaResolutions.push("1024");
+   return layamaResolutions;
+}
+
+function getOnScreenLogoUsage()
+{
+   return 5;
+}
+
+function getLayamaControls()
+{
+   return {defMove: false, defRot: 1, altMove: true, altRot: 2};
+}
+
+function getLayamaAlternatives()
+{
+   var layamaAlternatives = [];
+   layamaAlternatives.push({n: "base", u: "./jpg/base/", i:"./jpg/sun.png"});
+   layamaAlternatives.push({n: "DAY", u: "./jpg/DAY/", i:"./jpg/moon.png"});
+   return layamaAlternatives;
+}
+
+function getLayamaHotspots()
+{
+   var layamaHotspots = [];
+   layamaHotspots.push({n: "TV", m: 1, u: "https://www.youtube.com/embed/LbqnFk8SFHA", i: "./hotspots/noun-play-button-7490381.png", p: {x:334.367, y:117.859, z:328.858}, d: 215});
+   layamaHotspots.push({n: "Chair", m: 1, u: "https://www.finnishdesignshop.it/it-it/prodotto/sedia-j104-faggio", i: "./hotspots/noun-chair-7490427.png", p: {x:316.591, y:73.8944, z:-396.63}, d: 220});
+   layamaHotspots.push({n: "Sofa", m: 1, u: "https://www.sediarreda.com/it/p-hamagsoc-hay-mags-soft-low-c3?art_var=02HAL10005A025PO0601SINI070000BE&utm_areaiso=ITA&gad_source=1&gclid=CjwKCAiAneK8BhAVEiwAoy2HYYfIeC-KfWba3UN6Z7rYyJ9OGWUHRSmMpnPkA_ftjGPUwqtz7tJUkxoCd8oQAvD_BwE", i: "./hotspots/noun-sofa-7489138.png", p: {x:49.6036, y:82.0961, z:-497.006}, d: 300});
+   layamaHotspots.push({n: "Coffeetable", m: 1, u: "https://www.taschen.com/it/books/architecture-design/43440/bauhaus-updated-edition/?forceCountry=DE&gad_source=1&gclid=CjwKCAiAneK8BhAVEiwAoy2HYUTUpuRAsWlBkwtQmijfebwUgZyyfg4uuknWDQ4RJkBPFbrXQS8AkRoCHYgQAvD_BwE", i: "./hotspots/noun-knowledge-6414925.png", p: {x:93.444, y:33.938, z:-359.055}, d: 180});
+   layamaHotspots.push({n: "Mirror", m: 1, u: "https://www.maisonsdumonde.com/IT/it/p/specchio-rotondo-in-metallo-nero-oe-121-cm-rias-231869.htm?utm_source=google&utm_medium=cpc&utm_campaign=SEA-GOO-B2C-INT-IT-IT-DCD-MDM-GEN-PMAX-DESTOCK&gad_source=1&gclid=CjwKCAiAneK8BhAVEiwAoy2HYRGlJ5bSBvJQeVveySxwjQkHJ-vIkLG9sAykpvWS1cazhTtEguPHeBoCHy8QAvD_BwE", i: "./hotspots/noun-mirror-7444189.png", p: {x:495.17, y:158.878, z:396.222}, d: 180});
+   layamaHotspots.push({n: "Bathroom Sink", m: 1, u: "https://www.ceramicagalassia.it/catalogo/smartb/lavabi/lavabo-45x45-cm-1.html", i: "./hotspots/noun-faucet-5015818.png", p: {x:-50.0598, y:102.895, z:559.761}, d: 242});
+   layamaHotspots.push({n: "Plan", m: 0, u: "/hotspots/acos-the-unfolding-layout.jpg", i: "./hotspots/noun-plans-7113024.png", p: {x:-175.591, y:170.459, z:415.782}, d: 211.2});
+   layamaHotspots.push({n: "Table", m: 1, u: "https://slh.com.au/products/grasshopper-rect-dining-table-200cm-oak", i: "./hotspots/noun-table-7002016.png", p: {x:358.376, y:80.9599, z:-309.84}, d: 280});
+   layamaHotspots.push({n: "Boiler", m: 1, u: "https://alessi.com/it/products/plisse-electric-kettle-european-plug", i: "./hotspots/noun-boiler-1965534.png", p: {x:183.764, y:136.049, z:-26.5963}, d: 180});
+   layamaHotspots.push({n: "Kitchen faucet", m: 1, u: "https://www.deltafaucet.com/kitchen/product/1959LF.html?srsltid=AfmBOop_OKaBjFXzvQExC_kxo6n-IZgPmS9d5_wjEPVFw_psf62ksKhx", i: "./hotspots/noun-faucet-5015818.png", p: {x:383.066, y:106.319, z:-95.5171}, d: 240});
+   return layamaHotspots;
+}
+
